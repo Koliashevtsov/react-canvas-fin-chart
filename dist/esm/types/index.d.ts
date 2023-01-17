1e1f2423d6c42e307d8e431e5f180266e90851dc
nikshevtsov5-virtual <nikshevtsov5@gmail.com>
2023-01-18 00:59:57 +0200

    release 1.0.2

--- dist/esm/types/index.d.ts
+++ dist/esm/types/index.d.ts
@@ -1,6 +1,6 @@
 import { Data, InputOptions } from 'canvas-fin-chart/dist/types';
 interface CanvasChartProps {
     data: Data;
-    options: InputOptions;
+    options?: InputOptions;
 }
 export { CanvasChartProps };
